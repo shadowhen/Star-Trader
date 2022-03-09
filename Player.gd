95e3594efc9fa0b101227b56e4d2057c6e9bc26d
Chris <c.m.clements18@gmail.com>
2022-03-09 17:07:52 -0600

    Chris: Updated ship movement, sprite layer, and ship orientation when moving

--- Player.gd
+++ Player.gd
@@ -1,17 +1,24 @@
 extends KinematicBody2D
 
-const SPEED = 250.0
-
 export var current_balance : int = 100
 
 var move_pressed = false
-var target_position = Vector2()
+
 var interact_planet = false
+
+# movement variables
+var destination = Vector2()
+var target_position = Vector2()
+var velocity = Vector2()
+var speed = 0
+var max_speed = 200
+var acceleration = 50
+var is_moving = false
 
 func _ready():
 	var planets = get_tree().get_nodes_in_group("planets")
 	for planet in planets:
-		planet.connect("click_move", self, "_update_target_position")
+		planet.connect("click_move", self, "_update_destination")
 
 	var trades = get_tree().get_nodes_in_group("trade")
 	for trade in trades:
@@ -21,8 +28,29 @@ func _unhandled_input(event):
 	if interact_planet and event.is_action_pressed("trade"):
 		$"../GUI/Trade/".visible = not $"../GUI/Trade/".visible
 
-func _update_target_position(target_position):
-	self.target_position = target_position
+# Updates the ships destination
+func _update_destination(target_position):
+	destination = target_position
+	is_moving = true
+
+# Moves the ship to the target destination at the set speed
+func _physics_process(delta):
+	_movement_loop(delta)
+		
+func _movement_loop(delta):
+	if is_moving == false:
+		speed = 0
+	else:
+		speed += acceleration * delta
+		if speed > max_speed:
+			speed = max_speed
+	
+	velocity = position.direction_to(destination) * speed
+	look_at(destination)
+	if position.distance_to(destination) > 5:
+		velocity = move_and_slide(velocity)
+	else:
+		is_moving = false
 	
 func _conduct_trade(cargo_type, currency, amount):
 	#print("traded %d for %s" % [amount, ItemTrade.get_cargo_str(cargo_type)])
@@ -30,14 +58,6 @@ func _conduct_trade(cargo_type, currency, amount):
 	# Calculate new balance
 	current_balance += currency
 	print("Current balance: %d" % current_balance)
-
-func _physics_process(delta):
-	var direction_vec = target_position - global_position
-	var direction_normal = direction_vec.normalized()
-	
-	if direction_vec.length() < 5:
-		return
-	move_and_collide(direction_normal * SPEED * delta)
 
 # This function implements both ship upgrades.  
 func _ship_upgrade(current_balance, current_ship_upgrade):
